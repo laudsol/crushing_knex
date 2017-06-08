7da6fa4deab62217cdae79ccd06d2063a5fb8544
Sol Laudon <blaudon1@gmail.com>
2017-06-08 07:08:19 -0600

    fix database

--- jobs2/index.js
+++ jobs2/index.js
@@ -4,13 +4,47 @@ const env = 'development';
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
 
-knex('employer')
-  .join('job_employer','employer.id','=','job_employer.employer_id')
-  .join('job','job_employer.job_id','=','job.id')
-  .join('job_skill','job_skill.job_id','=','job.id')
+// knex('employer')
+//   .join('job_employer','employer.id','=','job_employer.employer_id')
+//   .join('job','job_employer.job_id','=','job.id')
+//   .join('job_skill','job_skill.job_id','=','job.id')
+//   .join('skill','job_skill.skill_id','=','skill.id')
+//   .select('skill.skill','employer.company')
+//   .where('skill','=','accounting')
+//   .then((result) => {
+//     console.log(result);
+//     knex.destroy();
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     knex.destroy();
+//     process.exit(1);
+//   });
+
+  // 1. name, description, and skill. Narrow out acountant
+
+    // knex('job')
+    //   .join('job_skill','job.id','=','job_skill.job_id')
+    //   .join('skill','job_skill.skill_id','=','skill.id')
+    //   .select('skill.skill','job.name','job.description')
+    //   // .where('skill.skill','=','accounting')
+    //   .then((result) => {
+    //     console.log(result);
+    //     knex.destroy();
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     knex.destroy();
+    //     process.exit(1);
+    //   });
+
+  //2. Find job name and description and skill by ascending min_exprnc
+
+  knex('job')
+  .join('job_skill','job.id','=','job_skill.job_id')
   .join('skill','job_skill.skill_id','=','skill.id')
-  .select('skill.skill','employer.company')
-  .where('skill','=','accounting')
+  .select('skill.skill','job.name','job.description', 'job.min_exprnc')
+  .orderBy('job.min_exprnc','asc')
   .then((result) => {
     console.log(result);
     knex.destroy();
--- jobs2/seeds/job.js
+++ jobs2/seeds/job.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('job').insert([
-        {id: 1, name: 'dev',description:'code stuff' , min_exprnc:1},
-        {id: 2, name: 'dev',description:'code things' , min_exprnc:4},
-        {id: 3, name: 'lawyer',description:'legal stuff' , min_exprnc:3},
-        {id: 4, name: 'lawer',description:'legal things' , min_exprnc:4},
-        {id: 5, name: 'accountant',description:'count stuff' , min_exprnc:1},
-        {id: 6, name: 'accountant',description:'count things' , min_exprnc:2}
+        {id: 1, name: 'fun dev',description:'code stuff' , min_exprnc:1},
+        {id: 2, name: 'boring dev',description:'code things' , min_exprnc:4},
+        {id: 3, name: 'fun lawyer',description:'legal stuff' , min_exprnc:3},
+        {id: 4, name: 'boring laywer',description:'legal things' , min_exprnc:4},
+        {id: 5, name: 'fun accountant',description:'count stuff' , min_exprnc:1},
+        {id: 6, name: 'boring accountant',description:'count things' , min_exprnc:2}
       ]);
     });
 };
